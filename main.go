c645b1fae3b32c9adf2e821c6aedf1f4a68a2bf0
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2022-09-19 21:17:07 -0700

    test pointer in Go

--- main.go
+++ main.go
@@ -3,25 +3,35 @@ import "fmt"
 
 type contactInfo struct {
 	email string  
-	zip int 
+	zipCode int 
 }
 
 type person struct {
 	firstName string
 	lastName string 
+	contact contactInfo
 }
 
-
 func main() {
-	//alex := person{firstName: "Alex", 
-	//			   lastName: "Anderson"}
-	//fmt.Println(alex)
-	var alex person 
+	jim := person{
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo{
+			email:"jim@gmail.com",
+			zipCode: 94000,
+		},
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	}
 	
-	fmt.Printf("%+v", alex)
+	jimPointer := &jim 
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
 
-	} 
+func (pointerToPerson *person) updateName(newFirstname string) {
+	(*pointerToPerson).firstName = newFirstname
+}
 
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
